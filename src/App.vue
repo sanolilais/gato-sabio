d50f9365a0cad56eb44347287fce7397adbd5ac2
Laís <87990551+sanolilais@users.noreply.github.com>
2023-01-11 03:27:45 +0000

    horcreation of the home component and the component that receives the quotes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
-  <main>
-    <h1>Gato Sábio App</h1>
-  </main>
+    <p class="monk-cat-quotes_title">Gato Sábio App</p>
+    <home />
+
 </template>
+
+<script>
+import home from './components/home.vue';
+import quotePicker from './components/quotePicker.vue';
+
+export default {  
+  components: {
+    home,
+    quotePicker
+  },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Acme&family=EB+Garamond:wght@600;700;800&display=swap');
+
+.monk-cat-quotes_title {
+  font-size: 35px;
+  text-align: center;
+  font-family: 'Acme', 'EB Garamond', serif;
+  font-weight: 800;
+  padding: 25px;
+  border-bottom: 2px solid #e4e1e7;
+  }
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div main class="monk-cat-quotes">
+        <img src="../assets/images/rdrama-marsey.gif" class="monk-cat-quotes__image"/>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+.monk-cat-quotes {
+    display: flex;
+    flex-direction: column;
+    margin: 50px;
+}
+.monk-cat-quotes__image {
+    height: 460px;
+    margin: auto;
+}
+
+</style>
